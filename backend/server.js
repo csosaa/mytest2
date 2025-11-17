1a61bdb5ee18b0836e4da4194ab43369e3694ea5
Carlos Sosa <casosa@technisys.com>
2025-11-17 17:43:26 -0600

    Fix server.js

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.send(`API is running in ${process.env.NODE_ENV}....`)
   );
 } else {
   const __dirname = path.resolve();
